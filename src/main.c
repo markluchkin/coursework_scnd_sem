be3d2e942de4da68e5b72ecf92ad00619d043420
Mark <142676023+markluchkin@users.noreply.github.com>
2024-05-03 14:37:14 +0300

    +rect

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include <png.h>
 #include <getopt.h>
 #include <unistd.h>
 #define PI 3.14159265
 
-struct Png{
+typedef struct Png{
     int width, height;
     png_byte color_type;
     png_byte bit_depth;
@@ -13,20 +15,34 @@ struct Png{
     png_infop info_ptr;
     int number_of_passes;
     png_bytep *row_pointers;
-};
+} Png;
 
 
 void printCWinfo();
 void printHelp();
 void read_png_file(char *file_name, struct Png *image);
 void write_png_file(char *file_name, struct Png *image);
-void drawRectangle();
-void rotateImage();
+int* getColor(char *color);
+void setPixel(Png *image, int *color, int x, int y);
+void drawLine(Png *image, int x1, int y1, int x2, int y2, char *thickness, int *color);
+void drawRectangle(Png *image, int x1, int y1, int x2, int y2, char *thickness, int *color, char *fill, int *fill_color);
+void drawOrnament(Png *image, char *pattern, int *color, char *thickness, int count);
+void rotateImage(Png *image, int x1, int y1, int x2, int y2, char *angle);
 
-int main(int argc, char** argv){
+
+int main(){
     printCWinfo();
+    char* input_file = "file.png";
+    char* output_file = "file2.png";
+    Png image;
+    read_png_file(input_file, &image);
+    char *color = "255.0.0";
+    int *arr = getColor(color);
+    drawRectangle(&image, 550, 200, 650, 100, "50", arr, "false", NULL);
+    write_png_file(output_file, &image);
 
     return 0;
+
 }
 
 void printCWinfo(){
@@ -104,7 +120,7 @@ void write_png_file(char *file_name, struct Png *image){
     int x, y;
     FILE* fp = fopen(file_name, "wb");
     if (!fp){
-                printf("Error: can't create file: %s\n", file_name);
+        printf("Error: can't create file: %s\n", file_name);
         fclose(fp);
         exit(0);
     }
@@ -154,11 +170,105 @@ void write_png_file(char *file_name, struct Png *image){
 
     for (y = 0; y < image->height; y++)
         free(image->row_pointers[y]);
+
     free(image->row_pointers);
     fclose(fp);
+    png_destroy_write_struct(&(image->png_ptr), &(image->info_ptr));
 
 }
 
-void drawRectangle(){}
+int *getColor(char *color) {
+    int *arr = malloc(sizeof(int) * 3);
+    sscanf(color, "%d.%d.%d", &arr[0], &arr[1], &arr[2]);
+    return arr;
+}
 
-void rotateImage(){}
+
+void drawSimpleCircle(Png *image,int x0, int y0, int radius, int *color){
+    int D = 3 - 2 * radius;
+    int x = 0;
+    int y = radius;
+    while (x <= y) {
+        setPixel(image, color, x+x0, y+y0);
+        setPixel(image, color, y+x0, x+y0);
+        setPixel(image, color, -y+x0, x+y0);
+        setPixel(image, color, -x+x0, y+y0);
+        setPixel(image, color, -x+x0, -y+y0);
+        setPixel(image, color, -y+x0, -x+y0);
+        setPixel(image, color, y+x0, -x+y0);
+        setPixel(image, color, x+x0, -y+y0);
+
+        if (D < 0) {
+            D += 4 * x + 6;
+            x++;
+        } else {
+            D += 4 * (x - y) + 10;
+            x++;
+            y--;
+        }
+    }
+}
+void checkThickness(char *thickness){
+    int line_thickness = atoi(thickness);
+    if (line_thickness <= 0){
+        printf("Error: thikness must be a positive integer\n");
+        exit(0);
+    }
+    return;
+}
+
+void setPixel(Png *image, int *color, int x, int y){
+    image->row_pointers[y][x * 3 + 0] = color[0];
+    image->row_pointers[y][x * 3 + 1] = color[1];
+    image->row_pointers[y][x * 3 + 2] = color[2];
+}
+
+void drawLine(Png *image, int x1, int y1, int x2, int y2, char *thickness, int *color){
+    checkThickness(thickness);
+    int line_thickness = atoi(thickness);
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int sx = x1 < x2 ? 1 : -1;
+    int sy = y1 < y2 ? 1 : -1;
+    int err = dx - dy;
+    int h = image->height;
+    int w = image->width;
+    while(1){
+        if (y1 >= 0 && y1 <= h && x1 >= 0 && x1 <= w){
+            if (line_thickness == 1){
+                setPixel(image, color, x1, y1);
+            }   
+        }
+
+        if(line_thickness > 1 && x1 - (line_thickness/2) < w && y1 - (line_thickness/2) < h && x1 + (line_thickness/2) >= 0 && y1 + (line_thickness/2) >= 0){
+            drawSimpleCircle(image, x1, y1, line_thickness/2 ,color);
+        }
+
+        if (x1 == x2 && y1 == y2){
+            break;
+        }
+
+        int e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+
+        if (e2 < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
+}
+
+
+void drawRectangle(Png *image, int x1, int y1, int x2, int y2, char *thickness, int *color, char *fill, int *fill_color){
+    drawLine(image, x1, y1, x2, y1, thickness, color);
+    drawLine(image, x1, y2, x2, y2, thickness, color);
+    drawLine(image, x2, y2, x2, y1, thickness, color);
+    drawLine(image, x1, y2, x1, y1, thickness, color);
+}
+
+void drawOrnament(Png *image, char *pattern, int *color, char *thickness, int count){}
+
+void rotateImage(Png *image, int x1, int y1, int x2, int y2, char *angle){}
